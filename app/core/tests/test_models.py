5eb744605c507276565c2e88c41d9ac1e2b14d7e
Coder5424 <crysis1228@mail.ru>
2023-07-16 19:05:00 +0300

    Added a test for creating a Tag model object, added a Tag model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,6 +7,11 @@ from decimal import Decimal
 from core import models
 
 
+def create_user(email='user@example.com', password='pass123'):
+    """Create and return new user"""
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTests(TestCase):
     """Test models"""
 
@@ -63,4 +68,17 @@ class ModelTests(TestCase):
             description='Recipe description'
         )
 
-        self.assertEqual(str(recipe), recipe.title)
+        self.assertEqual(
+            str(recipe),
+            recipe.title
+        )
+
+    def test_create_tag(self):
+        """Test creating tag is successful"""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(
+            str(tag),
+            tag.name
+        )
