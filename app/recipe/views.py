56d492b39e922a5c6301bf5faebc4d2d80a84f44
Coder5424 <crysis1228@mail.ru>
2023-07-26 22:28:07 +0300

    Implemented ingredient listing API

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -8,6 +8,7 @@ from rest_framework.permissions import IsAuthenticated
 from core.models import (
     Recipe,
     Tag,
+    Ingredient,
 )
 from recipe import serializers
 
@@ -39,7 +40,7 @@ class TagViewSet(
             mixins.ListModelMixin,
             mixins.DestroyModelMixin,
             viewsets.GenericViewSet):
-    """Manage tag in database"""
+    """Manage tags in database"""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
     authentication_classes = [TokenAuthentication]
@@ -48,3 +49,16 @@ class TagViewSet(
     def get_queryset(self):
         """Retrieve tags for authenticated user"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Manage ingredients in database"""
+    serializer_class = serializers.TagSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter ingredients for authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
